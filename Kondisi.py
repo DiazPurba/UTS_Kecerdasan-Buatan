55e3f81a1aa605232eb3a7af65d348c598ca0ff2
DiazPurba <104213920+DiazPurba@users.noreply.github.com>
2022-04-22 22:49:24 +0700

    Update Kondisi.py

--- Kondisi.py
+++ Kondisi.py
@@ -19,12 +19,12 @@ if(nilai > 10):
 
 #Kondisi if else adalah jika kondisi bernilai TRUE maka akan dieksekusi pada if, tetapi jika bernilai FALSE maka akan dieksekusi kode pada else
 
-nilai = 3
+kkm = 75
 #Jika pernyataan pada if bernilai TRUE maka if akan dieksekusi, tetapi jika FALSE kode pada else yang akan dieksekusi.
-if(nilai > 7):
+if(kkm > 70):
     print("Selamat Anda Lulus")
 else:
-    print("Maaf Anda Tidak Lulus")
+    print("Maaf Anda Remedial")
 
 
 
